2b3df5dceaef9b018f144a02b6f3c93f1e59a947
dupres <stephen.dupre@hotmail.fr>
2017-10-16 10:47:36 +0200

    Correcting td1

--- td1.py
+++ td1.py
@@ -23,7 +23,10 @@ def average_above_zero(pList):
         else:
             pNegativeCount = pNegativeCount +1
         pSum = pSum + pList[i]
-    pAverage = pSum / i
+    if i <> 0:
+        pAverage = pSum / i
+    else:
+        pAverage = pSum
     print('There is '+str(pPositiveCount)+' positive values and '+str(pNegativeCount)+' negative values in this array. The average is '+str(pAverage)+'.')
     return [pPositiveCount,pNegativeCount,pAverage]
 
@@ -143,11 +146,11 @@ def roi_bbox(pMatrix):
                 coordXTopLeft = i
             if coordYTopLeft == 0 and pMatrix[i,j]==1:
                 coordYTopLeft = j
-            i=i+1
             j=j+1
             if coordXTopLeft != 0 and coordYTopLeft != 0:
                 i = pWidth
                 j = pHeight
+        i=i+1
     i=pWidth
     j=pHeight
     while i > 0:
@@ -156,11 +159,11 @@ def roi_bbox(pMatrix):
                 coordXBottomRight = i
             if coordYBottomRight == 0 and pMatrix[i,j]==1:
                 coordYBottomRight = j
-            i=i-1
             j=j-1
             if coordXBottomRight != 0 and coordYBottomRight != 0:
                 i = 0
                 j = 0
+        i=i-1
     
     # while i < pWidth:
         # while j < pHeight:
